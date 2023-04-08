09c202c896b9a1d76db123a086cdfd0585d2c4e7
luca-piccinin <piccinin.luca.2002@gmail.com>
2023-04-08 16:21:53 +0200

    A

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         "template_1t76uxq",
         {
           form_name: form.name,
-          to_name: "Liron",
+          to_name: "Luca",
           from_email: form.email,
-          to_email: "contact@mail.com",
+          to_email: "piccinin.luca.2002@gmail.com",
           message: form.message,
         },
         "Jqq9AvwIuSjoMiA5c"
