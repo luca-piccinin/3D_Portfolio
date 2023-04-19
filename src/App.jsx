e40ebeb3122779ee52834eabd44c614b92476ef4
luca-piccinin <piccinin.luca.2002@gmail.com>
2023-04-19 14:48:34 +0200

    A

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDLIQpy0unGnVYth_m2bCB1uCp73Z7_u34",
-  authDomain: "portfolio-9e5f5.firebaseapp.com",
-  databaseURL: "https://portfolio-9e5f5-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "portfolio-9e5f5",
-  storageBucket: "portfolio-9e5f5.appspot.com",
-  messagingSenderId: "69867799247",
-  appId: "1:69867799247:web:bdf4793b24a311e6e2adba",
-  measurementId: "G-VFH8MNZ3FD"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 import { BrowserRouter } from "react-router-dom";
 
 import {
